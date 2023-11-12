173b40476a18566bd62c960f6bc2fad736944f40
cole.giannotti <colegiannotti@gmail.com>
2023-11-12 12:36:16 -0500

    Display Costs

---
+++ WebApplication1/Controllers/CostController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class CostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CostController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var costs = _context.Costs.ToList();
+            return View(costs);
+        }
+    }
+}
